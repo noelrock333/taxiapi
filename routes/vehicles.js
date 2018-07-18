c912454a3ae877b944c9bef1ef757971c00d89e0
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-07-18 11:09:49 -0500

    adding validations to vehicles

--- routes/vehicles.js
+++ routes/vehicles.js
@@ -5,9 +5,10 @@ const SHA256 = require('crypto-js/sha256');
 const authToken = require('../lib/auth-token');
 const helpers = require('../lib/helpers');
 const Vehicle = require('../models/vehicle');
+const vehicleValidation = require('../validations/models/vehicle');
 
 
-router.post('/', async (req, res, next) => {
+router.post('/', vehicleValidation.validate, async (req, res, next) => {
   let {organization, license_plate, number, model, year} = req.body;
   const vehicle = await new Vehicle({
     organization, license_plate, number, model, year
@@ -25,7 +26,7 @@ router.get('/:id', async (req, res, next) => {
   res.status(200).json(vehicle ? vehicle.toJSON() : { errors: { message: 'No se pudo encontrar el vehiculo' }});
 });
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', vehicleValidation.validate, async (req, res, next) => {
   const id = req.params.id;
   let {organization, license_plate, number, model, year} = req.body;
   let vehicle = await new Vehicle({id}).fetch();
