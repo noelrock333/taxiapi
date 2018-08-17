d6ce3b5587eadaad5514ffcf967093e43a908c9b
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-08-17 18:46:38 -0500

    adding new body parser config

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.use(logger('dev'));
 app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '5mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
 app.use(cookieParser());
 app.use(function(req, res, next){
   res.io = io;
