8fc9473cfa492159d0a50342ad633d3a3cd46b5c
Salvador Jimenez Mendoza <salvador@michelada.io>
2018-08-21 16:47:45 -0500

    adding drivers routes

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const authToken = require('../lib/auth-token');
 const helpers = require('../lib/helpers');
 const User = require('../models/user');
+const Driver = require('../models/driver');
 
-// Users routes
+// User routes
 
 router.get('/users', async (req, res, next) => {
   const users = await new User().fetchAll();
@@ -27,7 +28,6 @@ router.put('/user/:id', async (req, res, next) => {
   let user = await new User({id: user_id}).fetch();
   if(user) {
     user = await user.save(req.body, {patch: true});
-    console.log
     res.status(200).json(user.toJSON());
   }
   else {
@@ -39,8 +39,68 @@ router.delete('/user/:id', async (req, res, next) => {
   const user_id = req.params.id;
   let user = await new User({id: user_id}).fetch();
   if(user) {
-    user = await user.save(req.body, {patch: true});
-    res.status(200).json(user.toJSON());
+    try {
+      user = await user.destroy();
+      res.status(200).json({flash: ['Usuario borrado exitosamente']});
+    }
+    catch (error){
+      switch (error.code){
+        case '23503':
+        res.status(400).json({errors: ['Este usuario esta referenciado en otra tabla']});
+        break;
+      }
+    }
+  }
+  else {
+    res.status(404).json({errors: ['Este usuario no existe']});
+  }
+});
+
+// Driver routes
+
+router.get('/drivers', async (req, res, next) => {
+  const drivers = await new Driver().fetchAll();
+  res.status(200).json(drivers.toJSON());
+});
+
+router.get('/driver/:id', async (req, res, next) => {
+  const driver_id = req.params.id;
+  const driver = await new Driver({id: driver_id}).fetch();
+  if (driver) {
+    res.status(200).json(driver.toJSON());
+  }
+  else {
+    res.status(404).json({errors: ['Este usuario no existe']});
+  }
+});
+
+router.put('/driver/:id', async (req, res, next) => {
+  const driver_id = req.params.id;
+  let driver = await new Driver({id: driver_id}).fetch();
+  if(driver) {
+    driver = await driver.save(req.body, {patch: true});
+    res.status(200).json(driver.toJSON());
+  }
+  else {
+    res.status(404).json({errors: ['Este usuario no existe']});
+  }
+});
+
+router.delete('/driver/:id', async (req, res, next) => {
+  const driver_id = req.params.id;
+  let driver = await new Driver({id: driver_id}).fetch();
+  if(driver) {
+    try {
+      driver = await driver.destroy();
+      res.status(200).json({flash: ['Usuario borrado exitosamente']});
+    }
+    catch (error){
+      switch (error.code){
+        case '23503':
+        res.status(400).json({errors: ['Este usuario esta referenciado en otra tabla']});
+        break;
+      }
+    }
   }
   else {
     res.status(404).json({errors: ['Este usuario no existe']});
